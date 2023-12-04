a45271c06ba7b81220910c1c167a126221bb8aad
ChangWeiming <changweiming@yeah.net>
2023-12-04 20:45:54 +0800

    Day 3: epoll

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,6 +1,20 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <cstring>
+#include <cstdio>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+
+const int MAX_BUFFER_SIZE = 1024;
+const int SERVER_PORT = 11111;
+const char* SERVER_IP = "127.0.0.1";
+
+int getline(char* buf, int size, FILE* fp){
+    fgets(buf, size, fp);
+    buf[strcspn(buf, "\n")] = '\0';
+    return 0;
+}
 
 int main(){
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -8,10 +22,44 @@ int main(){
     bzero(&serv_addr, sizeof(serv_addr));
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(11111);
+    serv_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    serv_addr.sin_port = htons(SERVER_PORT);
 
     //连接服务器
-    connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));
+    int rc = 0;
+    rc = connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));
+    if(rc == -1){
+        //可能是ECONNREFUSED 或者 ETIMEDOUT
+        printf("Connect Server Error");
+        exit(-1);
+    }
+    printf("Connect Server Successfully fd:%d, server IP: %s, port: %d\n", sockfd, 
+                inet_ntoa(serv_addr.sin_addr), ntohs(serv_addr.sin_port));
+    
+    while(true){
+        char buf[MAX_BUFFER_SIZE];
+        memset(buf, 0, sizeof(buf));
+        //scanf("%s", buf);
+        getline(buf, sizeof(buf), stdin);
+        ssize_t write_bytes = write(sockfd, buf, sizeof(buf));
+        if(write_bytes == -1){          //write返回-1，表示发生错误
+            printf("socket already disconnected, can't write any more!\n");
+            break;
+        }
+        memset(buf, 0, sizeof(buf));
+        ssize_t read_bytes = read(sockfd, buf, sizeof(buf));
+        if(read_bytes == -1){
+            close(sockfd);
+            printf("Read Server Error\n");
+            exit(-1);
+        }
+        else if(read_bytes == 0){
+            printf("server socket disconnected!\n");
+            break;
+        }
+        else{
+            printf("server: %s\n", buf);
+        }
+    }
     return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,12 +1,41 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <cstring>
-#include <iostream>
 #include <unistd.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <iostream>
+
+const int MAX_BUFFER_SIZE = 1024;
+const int SERVER_PORT = 11111;
+const char* SERVER_IP = "127.0.0.1";
+const int MAX_EVENTS = 128;
+
 void errif(bool condition, const char* err_msg){
     if(condition){
         perror(err_msg);
         exit(EXIT_FAILURE);
+    }
+}
+
+int handle_event(int client_sockfd){
+    char buf[MAX_BUFFER_SIZE];
+    bzero(buf, sizeof(buf));
+    ssize_t read_bytes = read(client_sockfd, buf, sizeof(buf));
+    if(read_bytes > 0){
+        printf("client: %s\n", buf);
+        write(client_sockfd, buf, sizeof(buf));
+        return 1;
+    }
+    else if(read_bytes == 0){
+        close(client_sockfd);
+        printf("Client disconnect\n");
+        return 0;
+    }
+    else{
+        close(client_sockfd);
+        printf("Error reading\n");
+        return -1;
     }
 }
 
@@ -22,8 +51,8 @@ int main(){
     memset(&serv_addr, 0, sizeof(serv_addr));// 初始化对象
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(11111);
+    serv_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    serv_addr.sin_port = htons(SERVER_PORT);
 
     rc = bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)); // 将套接字fd绑定到端口
     errif(rc == -1, "Bind Socket Error");
@@ -31,19 +60,47 @@ int main(){
     // 2. 开启监听
     rc = listen(sockfd, SOMAXCONN);
     errif(rc == -1, "Listen Socket Error");
-
-    // 3. 从监听队列取出连接请求
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
-    memset(&client_addr, 0, sizeof(client_addr));
     
-    int client_sockfd = accept(sockfd, (sockaddr*)&client_addr, &client_addr_len); // accept 会发生阻塞。
-    if((rc = client_sockfd) == -1, "Accept Socket Error");
+    // 2.5 创建epoll
+    int epfd = epoll_create(1024);
+    errif(epfd == -1, "Creating Epoll Error");
 
-    printf("New client fd %d, IP: %s, Port: %d\n", client_sockfd, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    struct epoll_event events[MAX_EVENTS], ev;
+    ev.events = EPOLLIN;
+    ev.data.fd = sockfd;
+    epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &ev);
+
+    // 3. 从监听队列取出连接请求->从epoll中取出就绪的文件描述符
     while(true){
-        sleep(10);
+        int nfds = epoll_wait(epfd, events, MAX_EVENTS, -1);
+        for(int i = 0; i < nfds; i++){
+            if(events[i].data.fd == sockfd){
+                struct sockaddr_in client_addr;
+                socklen_t client_addr_len = sizeof(client_addr);
+                int client_sockfd = accept(sockfd, (sockaddr*)&client_addr, &client_addr_len); // accept 会发生阻塞。
+                if((rc = client_sockfd) == -1, "Accept Socket Error\n");
+                printf("New client fd %d, IP: %s, Port: %d\n", client_sockfd, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                ev.data.fd = client_sockfd;
+                ev.events = EPOLLIN | EPOLLET;
+                fcntl(client_sockfd, F_SETFL, fcntl(client_sockfd, F_GETFL) | O_NONBLOCK);
+                epoll_ctl(epfd, EPOLL_CTL_ADD, client_sockfd, &ev);
+            }
+            else if(events[i].events & EPOLLIN){
+                handle_event(events[i].data.fd);
+            }
+        }
     }
+    // struct sockaddr_in client_addr;
+    // socklen_t client_addr_len = sizeof(client_addr);
+    // memset(&client_addr, 0, sizeof(client_addr));
+    // sleep(20);
+    // int client_sockfd = accept(sockfd, (sockaddr*)&client_addr, &client_addr_len); // accept 会发生阻塞。
+    // if((rc = client_sockfd) == -1, "Accept Socket Error\n");
+    // printf("New client fd %d, IP: %s, Port: %d\n", client_sockfd, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
     
+    // while(true){
+    //     //sleep(10);
+    //     //这里写建立连接后的接受处理程序
+    // }
     return 0;
 }
